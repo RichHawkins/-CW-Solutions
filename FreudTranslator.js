3e95772786179d6f5cc5aea6d978791c72cb50ed
RichHawkins <101023519+RichHawkins@users.noreply.github.com>
2022-04-28 17:52:09 -0400

    codewars solve

---
+++ FreudTranslator.js
@@ -0,0 +1,5 @@
+function toFreud(string) {
+    return string === '' ? '' : string.split(' ').map(word => word = 'sex').join(' ')
+    }
+
+    console.log(toFreud("This is a test"))
---
+++ shiftyclosures.js
@@ -0,0 +1,9 @@
+var name = 'Abe';
+var greet_abe = function() {
+  return "Hello, " + name + '!';
+};
+
+var greet_ben = function() {
+  let name = 'Ben'; 
+  return "Hello, " + name + '!';
+};
